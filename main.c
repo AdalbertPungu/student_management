6759039b553e8dba99118c20bfed063ee3bc3a4e
Adalbert Pungu <18pw419@esisalama.org>
2021-09-28 18:05:26 +0200

    ajout du procedure menu 1

--- main.c
+++ main.c
@@ -24,3 +24,32 @@ struct Etudiants
 
 struct Etudiants info;
 
+//procedure menu 1 pour l'ajouter
+void menu1()
+{
+    FILE* fichier = NULL;
+    int nbreP;
+
+
+    //Dans la variable fichier on ajoute le fichier en mode lecture(a)
+    fichier = fopen("etudiant_data.txt", "a");
+
+    printf("\n======== MENU 1 =========\n");
+
+    do{
+        printf("\nQuel est le nombre des personnes ?: ");
+        scanf("%d", &nbreP);
+        if(nbreP <= 0)
+        {
+            printf("le nombre doit etre superieur a 1\n");
+            continue;
+        }
+        else if(nbreP > 0 && nbreP < 1)
+        {
+            break;
+        }
+    }while(nbreP <= 0);
+
+
+
+}
