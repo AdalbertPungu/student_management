e158943d041acd042e185d5598f7418e1fceb41a
Adalbert Pungu <18pw419@esisalama.org>
2021-09-28 17:51:53 +0200

    creation des structures

---
+++ main.c
@@ -0,0 +1,23 @@
+//structure tuteurs
+struct Tuteurs
+{
+    int numTuteur;
+    char nomTuteur[50];
+    char Profession[50];
+    char TelTuteur[10];
+};
+
+struct Tuteurs parent;
+
+//structure etudiants
+struct Etudiants
+{
+    char codeEtu[6];
+    char Nom[50];
+    char Post_nom[50];
+    char Prenom[50];
+    int age;
+    char adresseEtu;
+    int Tel[10];
+    //numTuteur
+};
